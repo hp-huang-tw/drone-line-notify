094c4e225e8478ae717e7100738c85775d6a3c16
stitchmos <stitchmos@hotmail.com>
2018-02-05 19:07:27 +0800

    read access token from drone secrets

--- send-notification.sh
+++ send-notification.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
+echo "Send LINE Notify with LINE_NOTIFY_ACCESS_TOKEN:"
+echo ${LINE_NOTIFY_ACCESS_TOKEN}
+echo "Message:"
+echo ${PLUGIN_MESSAGE}
+
+echo "Execute POST to LINE NOTIFY.."
 curl \
   -X POST \
-  -H "Authorization: Bearer ${PLUGIN_ACCESS_TOKEN}" \
+  -H "Authorization: Bearer ${LINE_NOTIFY_ACCESS_TOKEN}" \
   -F "message=${PLUGIN_MESSAGE}" \
   https://notify-api.line.me/api/notify
